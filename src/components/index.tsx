f9d73416d24179d759c2b9bd479de7da2b335c29
alicjamarciniak <alicja.marciniak@autochat.ai>
2025-01-15 20:37:07 +0100

    Adds basic shadcn components

---
+++ src/components/index.tsx
@@ -0,0 +1,4 @@
+export { default as Footer } from './Footer';
+export { StickyNav } from './sticky-nav';
+export { default as NumberInput } from './NumberInput';
+export { default as DatePicker } from './DatePicker';
